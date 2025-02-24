ebd8f2f7579df1a525ca6152fb5e51b666557f04
LooksLikeIveLost <ZeldaSpider599@gmail.com>
2025-02-24 14:29:17 -0700

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,12 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  user_id VARCHAR(255) UNIQUE NOT NULL,
+  tier VARCHAR(255)
+);
+
 CREATE TABLE bots (
   id SERIAL PRIMARY KEY,
-  owner_id VARCHAR(255) NOT NULL,
+  owner_id INTEGER NOT NULL,
   server_id VARCHAR(255) UNIQUE NOT NULL,
   name VARCHAR(255) NOT NULL,
   character_description TEXT,
@@ -8,23 +14,25 @@ CREATE TABLE bots (
   voice_description TEXT,
   voice_id VARCHAR(255),
   profile_picture_url TEXT,
-
-  UNIQUE (owner_id, server_id)
+  
+  UNIQUE (owner_id, server_id),
+  FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE webhooks {
+CREATE TABLE webhooks (
   id SERIAL PRIMARY KEY,
-  owner_id VARCHAR(255) NOT NULL,
   server_id VARCHAR(255) NOT NULL,
   webhook_id VARCHAR(255) NOT NULL,
   webhook_url TEXT,
   channel_id VARCHAR(255),
 
-  UNIQUE (owner_id, server_id, channel_id)
-}
+  UNIQUE (server_id, channel_id)
+);
 
-CREATE TABLE users {
-  id SERIAL PRIMARY KEY,
-  user_id VARCHAR(255) UNIQUE NOT NULL,
-  tier VARCHAR(255)
-}
+CREATE TABLE bots_webhooks (
+  bot_id INTEGER NOT NULL,
+  webhook_id INTEGER NOT NULL,
+  PRIMARY KEY (bot_id, webhook_id),
+  FOREIGN KEY (bot_id) REFERENCES bots(id) ON DELETE CASCADE,
+  FOREIGN KEY (webhook_id) REFERENCES webhooks(id) ON DELETE CASCADE
+);
